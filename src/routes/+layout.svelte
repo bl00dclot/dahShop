891ca7bb7e74c1d0cc04850a583818a090083f72
bl00dclot <discryptment@tutamail.com>
2025-11-22 13:06:46 +0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { DarkMode } from 'flowbite-svelte';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<DarkMode />
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Hero from '$lib/components/Hero.svelte';
+	import ProductGrid from '$lib/components/ui/ProductGrid.svelte';
+	import Testimonial from '$lib/components/Testimonial.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<Hero />
+<ProductGrid />
+<Testimonial />
+<Footer />
